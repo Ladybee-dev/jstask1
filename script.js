7345ddc71b2497dbd00167ce115a46a43faae465
Ladybee-dev <bosemwegie2019@gmail.com>
2022-06-22 11:02:19 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.write("<h1>introduction to javascript</h1>");
+document.write("<h2> Name: Blessing Osemwegie</h2>");
+document.write("<h3> Height: 1.70</h3>");
+document.write("<h3> country: Nigeria");
